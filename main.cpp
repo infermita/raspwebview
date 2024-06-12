0918f8e7f40aeec10d658b8d5316e9a1d53729ce
Okrim <infermita@gmail.com>
2024-06-12 17:27:08 +0200

    Boo

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 
 class MyWebEnginePage : public QWebEnginePage{
 protected:
-    void javaScriptConsoleMessage(QWebEnginePage::JavaScriptConsoleMessageLevel level,
+    void javaScriptConsoleMessage(JavaScriptConsoleMessageLevel level,
                                   const QString &message,
                                   int lineNumber,
                                   const QString &sourceID) override{
-        qDebug()<<"message: "<<message;
-        qDebug()<<"level: "<<level;
+        qDebug()<<"message: " << message << " Line: " << lineNumber << "SourceID " << sourceID;
+        //qDebug()<<"level: "<<level;
 
     }
 };
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
 
     //MainWindow w;
     //w.show();
+    /*
     QWebView view;
     view.settings()->setAttribute(QWebSettings::PluginsEnabled, true);
     view.settings()->setAttribute(QWebSettings::AcceleratedCompositingEnabled, true);
@@ -33,28 +34,29 @@ int main(int argc, char *argv[])
     view.settings()->setAttribute(QWebSettings::JavascriptEnabled, true);
     view.settings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
 
+    MyWebEnginePage *myPage = new MyWebEnginePage;
+    myPage->setParent(&view);
+    view.setPage(myPage);
+
+
     view.setFixedHeight(720);
     view.setFixedWidth(1280);
     view.load(QUrl("http://localhost:8000"));
 
-    view.setWindowFlags(view.windowFlags() | Qt::FramelessWindowHint);
+    //view.setWindowFlags(view.windowFlags() | Qt::FramelessWindowHint);
 
     view.show();
 
-    /*
+
+    */
     QWebEngineView view;
-    view.settings()->setAttribute(QWebSettings::PluginsEnabled, true);
-    view.settings()->setAttribute(QWebSettings::AcceleratedCompositingEnabled, true);
-    view.settings()->setAttribute(QWebSettings::WebGLEnabled, true);
-    view.settings()->setAttribute(QWebSettings::JavascriptEnabled, true);
-    view.settings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
-    //MyWebEnginePage *myPage = new MyWebEnginePage;
-    //myPage->setParent(&view);
-    //view.setPage(myPage);
+    MyWebEnginePage *myPage = new MyWebEnginePage;
+    myPage->setParent(&view);
+    view.setPage(myPage);
     view.setUrl(QUrl("http://localhost:8000"));
     view.setFixedHeight(720);
     view.setFixedWidth(1280);
     view.show();
-    */
+
     return a.exec();
 }
